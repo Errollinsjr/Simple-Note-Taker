651a2271d5126f7e28733ed54e0be857747140a6
Errollinsjr <errollins.jr@gmail.com>
2021-07-13 14:21:55 -0400

    added static and other dependencies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 // Series of npm packages that we will use to give our server useful functionality
 
 const express = require('express');
+const fs = require("fs");
+const path = require('path');
 
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -14,12 +16,11 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
-require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 // LISTENER
